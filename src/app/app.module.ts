45053f5313fbc3e8807a946ceffd8aec22975eb3
ItamarGronich <ItamarGronich@Gmail.com>
2017-07-04 19:15:45 +0300

    add basic layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    HeroComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
