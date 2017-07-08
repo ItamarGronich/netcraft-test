32480f22d94d0ced4303d06be7ce9019dddbee92
ItamarGronich <ItamarGronich@Gmail.com>
2017-07-08 13:27:25 +0300

    Remove unnecessary dep s.

--- src/app/components/twitter/twitter.service.ts
+++ src/app/components/twitter/twitter.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Jsonp } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 
@@ -8,7 +8,7 @@ export class TwitterService {
 
   oAuth: string;
 
-  constructor(private http: Http, private jsonp: Jsonp) { }
+  constructor(private http: Http) { }
 
   get(path, params) {
     const url = 'http://localhost:3001/' + path;
